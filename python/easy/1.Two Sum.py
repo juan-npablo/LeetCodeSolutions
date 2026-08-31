bb5b1c79bfd9afcf6420626280327cfed4887279
Juan P. Cataño O. <juanpablocatano19@gmail.com>
2026-08-30 22:00:24 -0500

    docs(problem-1-py): adjust info about complexity

--- python/easy/1.Two Sum.py
+++ python/easy/1.Two Sum.py
@@ -35,7 +35,7 @@ Only one valid answer exists.
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 """
 
-# Solution 1 - Brute Force
+# Solution 1 - Brute Force -> O(n^2) double loop to check all pairs of numbers
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
@@ -43,14 +43,14 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     return [i, j]
                 
-# Solution 2 - Verifying if target - nums[i] is in the list
+# Solution 2 - Verifying if target - nums[i] is in the list -> O(n^2) .index() method is O(n) and we are calling it inside a loop
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             if target - nums[i] in nums[i+1:]:
                 return [i, nums.index(target - nums[i], i+1)]
 
-#Solution 4
+#Solution 3 - Using a dictionary to store the indices of the numbers -> O(n)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dictionary = {}
